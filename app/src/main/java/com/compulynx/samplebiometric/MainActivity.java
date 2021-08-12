c6fd1e0faa9f9483db25f50f8f6ad935b16410d8
schixor88 <kushagra.acharya@outlook.com>
2021-08-12 14:48:15 +0545

    odk compatible

--- app/src/main/java/com/compulynx/samplebiometric/MainActivity.java
+++ app/src/main/java/com/compulynx/samplebiometric/MainActivity.java
@@ -1,16 +1,26 @@
 package com.compulynx.samplebiometric;
 
+import android.content.ClipData;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.compulynx.secugensaralbiometric.SecugenReader;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView mImageViewFingerprint, mImageViewRegister, mImageViewVerify;
 
     private String registerFpData = "";
+
+    private Bitmap bitmapS = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,23 +67,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v == mButtonCapture) {
-            secugenReader.captureFingerPrint((bitmap) -> mImageViewFingerprint.setImageBitmap(bitmap));
-        } else if (v == this.mButtonRegister) {
-            this.mCheckBoxMatched.setChecked(false);
-            secugenReader.captureFingerPrintWithEncodeData(new SecugenReader.FingerPrintDataCallback() {
+            bitmapS = null;
+            secugenReader.captureFingerPrint(new SecugenReader.FingerPrintCallback() {
                 @Override
-                public void onFingerPrintCapture(Bitmap bitmap, String encodedCaptureData, int captureQuality) {
+                public void onFingerPrintCapture(Bitmap bitmap) {
+                    bitmapS = bitmap;
                     mImageViewFingerprint.setImageBitmap(bitmap);
-                    mImageViewRegister.setImageBitmap(bitmap);
-                    mTextViewResult.setText("Click Verify");
-                    registerFpData = encodedCaptureData;
-                }
-
-                @Override
-                public void onFingerPrintQualityError(int captureQuality) {
-
                 }
             });
+        } else if (v == this.mButtonRegister) {
+//            this.mCheckBoxMatched.setChecked(false);
+//            secugenReader.captureFingerPrintWithEncodeData(new SecugenReader.FingerPrintDataCallback() {
+//                @Override
+//                public void onFingerPrintCapture(Bitmap bitmap, String encodedCaptureData, int captureQuality) {
+//                    mImageViewFingerprint.setImageBitmap(bitmap);
+//                    mImageViewRegister.setImageBitmap(bitmap);
+//                    mTextViewResult.setText("Click Verify");
+//                    registerFpData = encodedCaptureData;
+//                }
+//
+//                @Override
+//                public void onFingerPrintQualityError(int captureQuality) {
+//
+//                }
+//            });
+
+            if (bitmapS==null){
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Alert");
+                alertDialog.setMessage("Are you sure you want to cancel the process?");
+                alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "CANCEL",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.setButton(
+                        AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+//                            Bitmap myLogo = BitmapFactory.decodeResource(getResources(), R.drawable.test);
+//                            Uri uri = getImageUri(context, myLogo);
+//                            Intent intent = new Intent();
+//                            intent.setClipData( ClipData.newRawUri(null, uri));
+//                            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//                            setResult(RESULT_OK, intent);
+                                finish();
+                            }
+                        }
+                );
+                alertDialog.show();
+            }else {
+                Uri uri = getImageUri(MainActivity.this, bitmapS);
+                Toast.makeText(this,bitmapS.toString(),Toast.LENGTH_LONG).show();
+                Intent intent = new Intent();
+                intent.setClipData( ClipData.newRawUri(null, uri));
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+
+
+            Toast.makeText(this, "Intent Return Here", Toast.LENGTH_SHORT).show();
+
         } else if (v == this.mButtonMatch) {
             secugenReader.verifyCaptureFingerPrint(registerFpData, new SecugenReader.FingerPrintVerifyCallback() {
                 @Override
@@ -94,4 +152,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             });
         }
     }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
 }
